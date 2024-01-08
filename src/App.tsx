478686bafaaddf45588f1a6034e254b41f1116f8
Fabianhenrick <fabianhenrick@gmail.com>
2024-01-08 12:17:39 -0300

    Global Styles fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-
-import {ThemeProvider} from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './routes/Router'
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./routes/Router";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-
-
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
     </ThemeProvider>
-    )
-    
-  
+  );
 }
-
