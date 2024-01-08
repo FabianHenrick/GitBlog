d7d7d46729b2f1ea4836832b2f6b0e8d4429d609
Fabianhenrick <fabianhenrick@gmail.com>
2024-01-08 13:25:17 -0300

    Header Structuration

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,11 @@
 import { HeaderContainer } from "./styles";
-
+import { terminal_logo } from "../../../public/terminal_logo.svg";
 export function Header() {
   return (
     <HeaderContainer>
       <img />
       <div>
-        <img src="../public/terminal-solid.svg" />
+        <img src={terminal_logo} />
         <h1>GitBlog</h1>
       </div>
       <img />
