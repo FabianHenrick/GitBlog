71cf9582c22620bb68efc638307519dd00fcf081
Fabianhenrick <fabianhenrick@gmail.com>
2024-01-10 13:21:42 -0300

    profile icons implementations

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -17,6 +17,7 @@ export const HeaderContainer = styled.div`
 `;
 
 export const HomeContainer = styled.div`
+  margin-bottom: 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
